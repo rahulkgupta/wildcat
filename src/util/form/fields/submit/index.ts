55cea11e205a91a3022c0277927765a6118f3808
Rahul Gupta <rahulkartgupta@gmail.com>
2020-05-10 18:38:08 -0700

    fix linting

--- src/util/form/fields/submit/index.ts
+++ src/util/form/fields/submit/index.ts
@@ -1,42 +1,40 @@
-import Field from "..";
+import Field from '..';
 
 /**
  * Extends {@link Field}
  */
 export default class SubmitField extends Field {
-    value: string
-    onSubmit: Function
+  value: string;
+  onSubmit: Function;
 
-    /**
-     * 
-     * @param id 
-     * @param data 
-     * @param onUpdate 
-     * @param onSubmit 
-     */
-    constructor(id: string, data: any, onUpdate:Function, onSubmit: Function ) {
-        super(id, data, onUpdate);
-        this.value = data.value ? data.value : '';
-        this.onSubmit = onSubmit
-    }
+  /**
+   *
+   * @param id
+   * @param data
+   * @param onUpdate
+   * @param onSubmit
+   */
+  constructor(id: string, data: any, onUpdate: Function, onSubmit: Function) {
+    super(id, data, onUpdate);
+    this.value = data.value ? data.value : '';
+    this.onSubmit = onSubmit;
+  }
 
-    toJSON() {
-        return {
-            id: this.id,
-            value: this.value,
-        }
-    }
+  toJSON() {
+    return {
+      id: this.id,
+      value: this.value,
+    };
+  }
 
-    /**
-     * @returns this.value
-     */
-    getValue() {
-        return this.value;
-    }
+  /**
+   * @returns this.value
+   */
+  getValue() {
+    return this.value;
+  }
 
-    submit() {
-        this.onSubmit();
-    }
-
-
+  submit() {
+    this.onSubmit();
+  }
 }
