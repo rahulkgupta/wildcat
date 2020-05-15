345ff900fd5bd0ba63914740156c08d5a0dd43af
Rahul Gupta <rahulkartgupta@gmail.com>
2020-05-15 14:56:09 -0700

    undo test changes

--- src/util/form/index.test.ts
+++ src/util/form/index.test.ts
@@ -1,5 +1,62 @@
 import Form from '@src/util/form';
 
-test('this is a test', () => {
-  expect(2).toBe(2);
+let form: Form;
+let onUpdate: Function;
+
+describe('Form', () => {
+  beforeEach(() => {
+    onUpdate = jest.fn();
+    form = new Form(
+      {
+        id: '1234',
+        fields: {
+          ssaf: {
+            label: 'label',
+            value: 'value',
+            error: 'error',
+            type: 'text',
+          },
+        },
+      },
+      {
+        onUpdate: onUpdate,
+      },
+    );
+  });
+
+  describe('constructor', () => {
+    test('creates a form', () => {
+      expect(form.getId()).toBe('1234');
+    });
+    test('throws if a bad field type is passed in', () => {
+      try {
+        new Form(
+          {
+            id: '123',
+            fields: {
+              sdfasdf: {
+                type: 'asdfasf',
+              },
+            },
+          },
+          {
+            onUpdate: jest.fn(),
+          },
+        );
+      } catch (e) {
+        expect(e.message).toBe('no field type found asdfasf');
+      }
+    });
+  });
+
+  describe('functions', () => {
+    test('updates and calls onUpdate', () => {
+      form.update();
+      expect(onUpdate).toHaveBeenCalled();
+    });
+
+    test('getFields', () => {
+      expect(form.getFields().length).toBe(1);
+    });
+  });
 });
