75131e4cccdb6813f8afbc117a5820adebd1ba06
Rahul Gupta <rahulkartgupta@gmail.com>
2020-05-29 15:09:51 -0700

    WIP

---
+++ src/db/client.ts
@@ -0,0 +1,13 @@
+import { GraphQLClient } from 'graphql-request';
+import { getSdk } from '@src/generated/test';
+
+console.log('hello world ${process.env.FAUNADB_SECRET}');
+const graphQLClient = new GraphQLClient(process.env.GRAPHQL_ENDPOINT || '', {
+  headers: {
+    authorization: `Bearer ${process.env.FAUNADB_SECRET}`,
+  },
+});
+
+const sdk = getSdk(graphQLClient);
+
+export default sdk;
--- src/pages/api/integrations/google/callback.ts
+++ src/pages/api/integrations/google/callback.ts
@@ -1,8 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import oauth2Client from '@src/util/integrations/google/auth';
+import client from '@src/db/client';
 
 // TODO: handle errors
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const code = req.query.code;
+  const { tokens } = await oauth2Client.getToken(req.query.code as string);
+  client.createAccessToken({
+    data: {
+      service: 'google',
+      refresh_token: tokens.refresh_token,
+      access_token: tokens.access_token,
+      expiry_date: tokens.expiry_date,
+      token_type: tokens.token_type,
+    },
+  });
 
   res.writeHead(302, {
     Location: '/',
