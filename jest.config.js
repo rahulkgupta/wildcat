35e5b3261a09ef6d94314deddc603585ccc555ac
Rahul Gupta <rahulkartgupta@gmail.com>
2020-06-01 16:07:45 -0700

    Fix test, build, lint errors

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   roots: ['<rootDir>'],
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](build|docs|node_modules|.next)[/\\\\]', 'src/db/sdk.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>[/\\\\](build|docs|node_modules|.next)[/\\\\]',
+    '<rootDir>[/\\\\]src[/\\\\]db[/\\\\]sdk.ts',
+  ],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   testEnvironment: 'jsdom',
   testURL: 'http://localhost',
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
-  collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['./src/**/*.{ts,tsx}', '!./src/db/(sdk|index).ts'],
   collectCoverage: true,
   coverageThreshold: {
     global: {
