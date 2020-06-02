64ce0cf7bc01d9dbf2f3a2307ab072319de15527
Rahul Gupta <rahul.gupta@affirm.com>
2020-06-01 20:40:56 -0700

    Db calls (#23)
    Use access token table to connect google sheets.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/camelcase": "off",
     "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".jsx"] }],
   },
   ignorePatterns: ["src/db/sdk.ts"]
