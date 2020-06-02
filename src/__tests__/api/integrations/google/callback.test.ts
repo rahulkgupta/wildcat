d8ee98de70e9f6c7663f76ba8710580cfb7909db
Rahul Gupta <rahulkartgupta@gmail.com>
2020-06-01 20:38:02 -0700

    fix tests

--- src/__tests__/api/integrations/google/callback.test.ts
+++ src/__tests__/api/integrations/google/callback.test.ts
@@ -1,14 +1,37 @@
 import callback from '@src/pages/api/integrations/google/callback';
 import { mock } from 'jest-mock-extended';
 import { NextApiRequest, NextApiResponse } from 'next';
+import db from '@src/db';
+import oauth2Client from '@src/util/integrations/google/auth';
+
+jest.mock('@src/util/integrations/google/auth');
+jest.mock('@src/db');
 
 describe('callback', () => {
   it('redirects', async () => {
     const res = mock<NextApiResponse>();
     const req = mock<NextApiRequest>();
+    req.query.code = 'code';
+    oauth2Client.getToken.mockReturnValueOnce({
+      tokens: {
+        refresh_token: 'test',
+        access_token: 'test',
+        expiry_date: 1234,
+        token_type: 'bearer',
+      },
+    });
 
     await callback(req, res);
-
+    expect(oauth2Client.getToken).toHaveBeenCalledWith('code');
+    expect(db.createAccessToken).toHaveBeenCalledWith({
+      data: {
+        service: 'google',
+        refresh_token: 'test',
+        access_token: 'test',
+        expiry_date: 1234,
+        token_type: 'bearer',
+      },
+    });
     expect(res.writeHead).toHaveBeenCalledWith(302, { Location: '/' });
   });
 });
