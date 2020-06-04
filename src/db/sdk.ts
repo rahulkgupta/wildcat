8040b46fb9a669228d3420254f89cb4ab59cfab7
Rahul Gupta <rahulkartgupta@gmail.com>
2020-06-04 14:02:37 -0700

    Undo extra db changes

--- src/db/sdk.ts
+++ src/db/sdk.ts
@@ -1,6 +1,7 @@
-import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+import { GraphQLClient } from 'graphql-request';
+import { print } from 'graphql';
+import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
-export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -8,194 +9,193 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
-  /** The `Upload` scalar type represents a file upload. */
-  Upload: any;
+  Date: any;
+  Long: any;
+  Time: any;
 };
 
-export type Field = {
-  __typename?: 'Field';
-  id: Scalars['ID'];
-  label?: Maybe<Scalars['String']>;
-  value?: Maybe<Scalars['String']>;
-  error?: Maybe<Scalars['String']>;
-  fieldType?: Maybe<Scalars['String']>;
+
+
+
+
+
+
+export type AccessToken = {
+  __typename?: 'AccessToken';
+  access_token?: Maybe<Scalars['String']>;
+  _id: Scalars['ID'];
+  refresh_token?: Maybe<Scalars['String']>;
+  token_type?: Maybe<Scalars['String']>;
+  service?: Maybe<Scalars['String']>;
+  expiry_date?: Maybe<Scalars['Float']>;
+  _ts: Scalars['Long'];
 };
 
-export type FormPointer = {
-  __typename?: 'FormPointer';
-  nextType?: Maybe<Scalars['Int']>;
-  id: Scalars['ID'];
+export type AccessTokenInput = {
+  access_token?: Maybe<Scalars['String']>;
+  refresh_token?: Maybe<Scalars['String']>;
+  expiry_date?: Maybe<Scalars['Float']>;
+  token_type?: Maybe<Scalars['String']>;
+  service?: Maybe<Scalars['String']>;
 };
 
-export type Form = {
-  __typename?: 'Form';
+
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createAccessToken: AccessToken;
+  updateAccessToken?: Maybe<AccessToken>;
+  deleteAccessToken?: Maybe<AccessToken>;
+};
+
+
+export type MutationCreateAccessTokenArgs = {
+  data: AccessTokenInput;
+};
+
+
+export type MutationUpdateAccessTokenArgs = {
   id: Scalars['ID'];
-  fields?: Maybe<Array<Maybe<Field>>>;
-  next?: Maybe<FormPointer>;
+  data: AccessTokenInput;
+};
+
+
+export type MutationDeleteAccessTokenArgs = {
+  id: Scalars['ID'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  form?: Maybe<Form>;
+  findAccessTokenByID?: Maybe<AccessToken>;
+  findAccessTokenByService?: Maybe<AccessToken>;
 };
 
 
-export type QueryFormArgs = {
+export type QueryFindAccessTokenByIdArgs = {
   id: Scalars['ID'];
 };
 
-export enum CacheControlScope {
-  Public = 'PUBLIC',
-  Private = 'PRIVATE'
-}
 
-
-export type WithIndex<TObject> = TObject & Record<string, any>;
-export type ResolversObject<TObject> = WithIndex<TObject>;
-
-export type ResolverTypeWrapper<T> = Promise<T> | T;
-
-
-export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
-  fragment: string;
-  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+export type QueryFindAccessTokenByServiceArgs = {
+  service?: Maybe<Scalars['String']>;
 };
 
-export type NewStitchingResolver<TResult, TParent, TContext, TArgs> = {
-  selectionSet: string;
-  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+
+export type FindAccessTokenByServiceQueryVariables = {
+  service: Scalars['String'];
 };
-export type StitchingResolver<TResult, TParent, TContext, TArgs> = LegacyStitchingResolver<TResult, TParent, TContext, TArgs> | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
-  | ResolverFn<TResult, TParent, TContext, TArgs>
-  | StitchingResolver<TResult, TParent, TContext, TArgs>;
 
-export type ResolverFn<TResult, TParent, TContext, TArgs> = (
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
-) => Promise<TResult> | TResult;
 
-export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
-) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+export type FindAccessTokenByServiceQuery = (
+  { __typename?: 'Query' }
+  & { findAccessTokenByService?: Maybe<(
+    { __typename?: 'AccessToken' }
+    & CredentialOutputFragment
+  )> }
+);
 
-export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
-) => TResult | Promise<TResult>;
+export type FindAccessTokenByIdQueryVariables = {
+  id: Scalars['ID'];
+};
 
-export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+
+export type FindAccessTokenByIdQuery = (
+  { __typename?: 'Query' }
+  & { findAccessTokenByID?: Maybe<(
+    { __typename?: 'AccessToken' }
+    & CredentialOutputFragment
+  )> }
+);
+
+export type CreateAccessTokenMutationVariables = {
+  data: AccessTokenInput;
+};
+
+
+export type CreateAccessTokenMutation = (
+  { __typename?: 'Mutation' }
+  & { createAccessToken: (
+    { __typename?: 'AccessToken' }
+    & CredentialOutputFragment
+  ) }
+);
+
+export type UpdateAccessTokenMutationVariables = {
+  id: Scalars['ID'];
+  data: AccessTokenInput;
+};
+
+
+export type UpdateAccessTokenMutation = (
+  { __typename?: 'Mutation' }
+  & { updateAccessToken?: Maybe<(
+    { __typename?: 'AccessToken' }
+    & CredentialOutputFragment
+  )> }
+);
+
+export type CredentialOutputFragment = (
+  { __typename?: 'AccessToken' }
+  & Pick<AccessToken, 'access_token' | 'refresh_token' | 'expiry_date' | 'token_type' | 'service' | '_id'>
+);
+
+export const CredentialOutputFragmentDoc = gql`
+    fragment credentialOutput on AccessToken {
+  access_token
+  refresh_token
+  expiry_date
+  token_type
+  service
+  _id
 }
-
-export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
-  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+    `;
+export const FindAccessTokenByServiceDocument = gql`
+    query findAccessTokenByService($service: String!) {
+  findAccessTokenByService(service: $service) {
+    ...credentialOutput
+  }
 }
-
-export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
-  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
-  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
-
-export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
-  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
-  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
-
-export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
-  parent: TParent,
-  context: TContext,
-  info: GraphQLResolveInfo
-) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
-
-export type IsTypeOfResolverFn<T = {}> = (obj: T, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
-
-export type NextResolverFn<T> = () => Promise<T>;
-
-export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
-  next: NextResolverFn<TResult>,
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
-) => TResult | Promise<TResult>;
-
-/** Mapping between all available schema types and the resolvers types */
-export type ResolversTypes = ResolversObject<{
-  String: ResolverTypeWrapper<Scalars['String']>;
-  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
-  Field: ResolverTypeWrapper<Field>;
-  ID: ResolverTypeWrapper<Scalars['ID']>;
-  FormPointer: ResolverTypeWrapper<FormPointer>;
-  Int: ResolverTypeWrapper<Scalars['Int']>;
-  Form: ResolverTypeWrapper<Form>;
-  Query: ResolverTypeWrapper<{}>;
-  CacheControlScope: CacheControlScope;
-  Upload: ResolverTypeWrapper<Scalars['Upload']>;
-}>;
-
-/** Mapping between all available schema types and the resolvers parents */
-export type ResolversParentTypes = ResolversObject<{
-  String: Scalars['String'];
-  Boolean: Scalars['Boolean'];
-  Field: Field;
-  ID: Scalars['ID'];
-  FormPointer: FormPointer;
-  Int: Scalars['Int'];
-  Form: Form;
-  Query: {};
-  CacheControlScope: CacheControlScope;
-  Upload: Scalars['Upload'];
-}>;
-
-export type FieldResolvers<ContextType = any, ParentType extends ResolversParentTypes['Field'] = ResolversParentTypes['Field']> = ResolversObject<{
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  label?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  value?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  error?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  fieldType?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-}>;
-
-export type FormPointerResolvers<ContextType = any, ParentType extends ResolversParentTypes['FormPointer'] = ResolversParentTypes['FormPointer']> = ResolversObject<{
-  nextType?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-}>;
-
-export type FormResolvers<ContextType = any, ParentType extends ResolversParentTypes['Form'] = ResolversParentTypes['Form']> = ResolversObject<{
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  fields?: Resolver<Maybe<Array<Maybe<ResolversTypes['Field']>>>, ParentType, ContextType>;
-  next?: Resolver<Maybe<ResolversTypes['FormPointer']>, ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
-}>;
-
-export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
-  form?: Resolver<Maybe<ResolversTypes['Form']>, ParentType, ContextType, RequireFields<QueryFormArgs, 'id'>>;
-}>;
-
-export interface UploadScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Upload'], any> {
-  name: 'Upload';
+    ${CredentialOutputFragmentDoc}`;
+export const FindAccessTokenByIdDocument = gql`
+    query findAccessTokenByID($id: ID!) {
+  findAccessTokenByID(id: $id) {
+    ...credentialOutput
+  }
 }
+    ${CredentialOutputFragmentDoc}`;
+export const CreateAccessTokenDocument = gql`
+    mutation createAccessToken($data: AccessTokenInput!) {
+  createAccessToken(data: $data) {
+    ...credentialOutput
+  }
+}
+    ${CredentialOutputFragmentDoc}`;
+export const UpdateAccessTokenDocument = gql`
+    mutation updateAccessToken($id: ID!, $data: AccessTokenInput!) {
+  updateAccessToken(id: $id, data: $data) {
+    ...credentialOutput
+  }
+}
+    ${CredentialOutputFragmentDoc}`;
 
-export type Resolvers<ContextType = any> = ResolversObject<{
-  Field?: FieldResolvers<ContextType>;
-  FormPointer?: FormPointerResolvers<ContextType>;
-  Form?: FormResolvers<ContextType>;
-  Query?: QueryResolvers<ContextType>;
-  Upload?: GraphQLScalarType;
-}>;
+export type SdkFunctionWrapper = <T>(action: () => Promise<T>) => Promise<T>;
 
 
-/**
- * @deprecated
- * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
- */
-export type IResolvers<ContextType = any> = Resolvers<ContextType>;
+const defaultWrapper: SdkFunctionWrapper = sdkFunction => sdkFunction();
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
+  return {
+    findAccessTokenByService(variables: FindAccessTokenByServiceQueryVariables): Promise<FindAccessTokenByServiceQuery> {
+      return withWrapper(() => client.request<FindAccessTokenByServiceQuery>(print(FindAccessTokenByServiceDocument), variables));
+    },
+    findAccessTokenByID(variables: FindAccessTokenByIdQueryVariables): Promise<FindAccessTokenByIdQuery> {
+      return withWrapper(() => client.request<FindAccessTokenByIdQuery>(print(FindAccessTokenByIdDocument), variables));
+    },
+    createAccessToken(variables: CreateAccessTokenMutationVariables): Promise<CreateAccessTokenMutation> {
+      return withWrapper(() => client.request<CreateAccessTokenMutation>(print(CreateAccessTokenDocument), variables));
+    },
+    updateAccessToken(variables: UpdateAccessTokenMutationVariables): Promise<UpdateAccessTokenMutation> {
+      return withWrapper(() => client.request<UpdateAccessTokenMutation>(print(UpdateAccessTokenDocument), variables));
+    }
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
